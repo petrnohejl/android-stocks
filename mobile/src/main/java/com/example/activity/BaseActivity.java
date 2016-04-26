f3129f4649f99923c8ae6ef4dc91647a43f4b646
Petr Nohejl <petr.nohejl@gmail.com>
2016-04-27 01:07:50 +0200

    BaseActivity

---
+++ mobile/src/main/java/com/example/activity/BaseActivity.java
@@ -0,0 +1,50 @@
+package com.example.activity;
+
+import android.os.Bundle;
+
+import eu.inloop.viewmodel.base.ViewModelBaseEmptyActivity;
+
+
+public abstract class BaseActivity extends ViewModelBaseEmptyActivity
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+	
+	
+	@Override
+	public void onStart()
+	{
+		super.onStart();
+	}
+	
+	
+	@Override
+	public void onResume()
+	{
+		super.onResume();
+	}
+	
+	
+	@Override
+	public void onPause()
+	{
+		super.onPause();
+	}
+	
+	
+	@Override
+	public void onStop()
+	{
+		super.onStop();
+	}
+	
+	
+	@Override
+	public void onDestroy()
+	{
+		super.onDestroy();
+	}
+}
--- mobile/src/main/java/com/example/activity/StockDetailActivity.java
+++ mobile/src/main/java/com/example/activity/StockDetailActivity.java
@@ -8,10 +8,8 @@ import android.support.v7.widget.Toolbar;
 
 import com.example.R;
 
-import eu.inloop.viewmodel.base.ViewModelBaseEmptyActivity;
 
-
-public class StockDetailActivity extends ViewModelBaseEmptyActivity
+public class StockDetailActivity extends BaseActivity
 {
 	public static Intent newIntent(Context context)
 	{
@@ -28,43 +26,8 @@ public class StockDetailActivity extends ViewModelBaseEmptyActivity
 		setContentView(R.layout.activity_stock_detail);
 		setupActionBar();
 	}
-	
-	
-	@Override
-	public void onStart()
-	{
-		super.onStart();
-	}
-	
-	
-	@Override
-	public void onResume()
-	{
-		super.onResume();
-	}
-	
-	
-	@Override
-	public void onPause()
-	{
-		super.onPause();
-	}
-	
-	
-	@Override
-	public void onStop()
-	{
-		super.onStop();
-	}
-	
-	
-	@Override
-	public void onDestroy()
-	{
-		super.onDestroy();
-	}
-	
-	
+
+
 	private void setupActionBar()
 	{
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
