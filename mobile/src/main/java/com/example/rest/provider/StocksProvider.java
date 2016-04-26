06f47fbaf3ae9ce184e10ee4b870dc9941f5b74b
Petr Nohejl <petr.nohejl@gmail.com>
2016-04-27 01:34:21 +0200

    StocksProvider

---
+++ mobile/src/main/java/com/example/rest/provider/StocksProvider.java
@@ -0,0 +1,48 @@
+package com.example.rest.provider;
+
+import com.example.entity.LookupEntity;
+import com.example.entity.QuoteEntity;
+import com.example.rest.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public class StocksProvider
+{
+	public static final String QUOTE_CALL_TYPE = "quote";
+	public static final String LOOKUP_CALL_TYPE = "lookup";
+
+	private static volatile StocksService sService;
+
+
+	public interface StocksService
+	{
+		@GET("Quote/{format}")
+		Call<QuoteEntity> quote(@Path("format") String format, @Query("symbol") String symbol);
+
+		@GET("Lookup/{format}")
+		Call<LookupEntity[]> lookup(@Path("format") String format, @Query("input") String input);
+	}
+
+
+	private StocksProvider() {}
+
+
+	public static StocksService getService()
+	{
+		if(sService == null)
+		{
+			synchronized(StocksProvider.class)
+			{
+				if(sService == null)
+				{
+					sService = RetrofitClient.createService(StocksService.class);
+				}
+			}
+		}
+		return sService;
+	}
+}
