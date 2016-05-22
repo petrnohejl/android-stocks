d7db274d60bb4cd1e0a6a503eb722b068c12aa22
Petr Nohejl <petr.nohejl@gmail.com>
2016-05-22 15:28:01 -0700

    BaseViewModel implements Observable

--- mobile/src/main/java/com/example/viewmodel/BaseViewModel.java
+++ mobile/src/main/java/com/example/viewmodel/BaseViewModel.java
@@ -1,5 +1,7 @@
 package com.example.viewmodel;
 
+import android.databinding.Observable;
+import android.databinding.PropertyChangeRegistry;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,8 +12,11 @@ import com.example.utility.Logcat;
 import eu.inloop.viewmodel.AbstractViewModel;
 
 
-public abstract class BaseViewModel<T extends BaseView> extends AbstractViewModel<T>
+public abstract class BaseViewModel<T extends BaseView> extends AbstractViewModel<T> implements Observable
 {
+	private transient PropertyChangeRegistry mCallbacks;
+
+
 	@Override
 	public void onCreate(@Nullable Bundle arguments, @Nullable Bundle savedInstanceState)
 	{
@@ -57,6 +62,45 @@ public abstract class BaseViewModel<T extends BaseView> extends AbstractViewMode
 	{
 		Logcat.d("");
 		super.onDestroy();
+	}
+
+
+	@Override
+	public synchronized void addOnPropertyChangedCallback(OnPropertyChangedCallback callback)
+	{
+		if(mCallbacks == null)
+		{
+			mCallbacks = new PropertyChangeRegistry();
+		}
+		mCallbacks.add(callback);
+	}
+
+
+	@Override
+	public synchronized void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback)
+	{
+		if(mCallbacks != null)
+		{
+			mCallbacks.remove(callback);
+		}
+	}
+
+
+	public synchronized void notifyChange()
+	{
+		if(mCallbacks != null)
+		{
+			mCallbacks.notifyCallbacks(this, 0, null);
+		}
+	}
+
+
+	public void notifyPropertyChanged(int fieldId)
+	{
+		if(mCallbacks != null)
+		{
+			mCallbacks.notifyCallbacks(this, fieldId, null);
+		}
 	}
 
 
