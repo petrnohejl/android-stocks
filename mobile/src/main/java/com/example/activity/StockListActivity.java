f73a5d0d1c04bfc468c691d55bdbd68201e0d177
Petr Nohejl <petr.nohejl@gmail.com>
2016-07-29 14:59:51 +0200

    Menu on main screen

--- mobile/src/main/java/com/example/activity/StockListActivity.java
+++ mobile/src/main/java/com/example/activity/StockListActivity.java
@@ -5,6 +5,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.R;
 
@@ -28,6 +31,32 @@ public class StockListActivity extends BaseActivity
 	}
 
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		// action bar menu
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.activity_stock_list, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		// action bar menu behavior
+		switch(item.getItemId())
+		{
+			case R.id.menu_activity_stock_list_hello:
+				startHelloWorldActivity();
+				return true;
+
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}
+
+
 	private void setupActionBar()
 	{
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -39,5 +68,12 @@ public class StockListActivity extends BaseActivity
 		bar.setDisplayShowHomeEnabled(true);
 		bar.setDisplayHomeAsUpEnabled(false);
 		bar.setHomeButtonEnabled(true);
+	}
+
+
+	private void startHelloWorldActivity()
+	{
+		Intent intent = HelloWorldActivity.newIntent(this);
+		startActivity(intent);
 	}
 }
