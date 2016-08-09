25007c779d006934714b9abe4f24d8a1ec80af62
Petr Nohejl <petr.nohejl@gmail.com>
2016-08-09 16:46:05 +0200

    Fix binding type in StockPagerAdapter

--- mobile/src/main/java/com/example/adapter/StockPagerAdapter.java
+++ mobile/src/main/java/com/example/adapter/StockPagerAdapter.java
@@ -4,12 +4,12 @@ import android.databinding.ObservableArrayList;
 
 import com.example.R;
 import com.example.adapter.base.SimpleDataBoundPagerAdapter;
-import com.example.databinding.FragmentStockPagerBinding;
+import com.example.databinding.FragmentStockPagerItemBinding;
 import com.example.ui.StockPagerView;
 import com.example.viewmodel.StockPagerViewModel;
 
 
-public class StockPagerAdapter extends SimpleDataBoundPagerAdapter<FragmentStockPagerBinding>
+public class StockPagerAdapter extends SimpleDataBoundPagerAdapter<FragmentStockPagerItemBinding>
 {
 	public StockPagerAdapter(StockPagerView view, StockPagerViewModel viewModel)
 	{
