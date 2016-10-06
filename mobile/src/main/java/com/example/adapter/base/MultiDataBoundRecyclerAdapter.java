4439f271a0ee79475703ecc523f57e731091d27d
Petr Nohejl <petr.nohejl@gmail.com>
2016-10-06 18:31:39 +0200

    Optimize MultiDataBoundRecyclerAdapter to use varargs

--- mobile/src/main/java/com/example/adapter/base/MultiDataBoundRecyclerAdapter.java
+++ mobile/src/main/java/com/example/adapter/base/MultiDataBoundRecyclerAdapter.java
@@ -11,29 +11,13 @@ import java.util.List;
 abstract public class MultiDataBoundRecyclerAdapter extends BaseDataBoundRecyclerAdapter
 {
 	private BaseView mView;
-	private ObservableArrayList<?> mItems1;
-	private ObservableArrayList<?> mItems2;
-	private ObservableArrayList<?> mItems3;
+	private ObservableArrayList<?>[] mItems;
 
 
-	public MultiDataBoundRecyclerAdapter(BaseView view, ObservableArrayList<?> items1)
-	{
-		this(view, items1, null, null);
-	}
-
-
-	public MultiDataBoundRecyclerAdapter(BaseView view, ObservableArrayList<?> items1, ObservableArrayList<?> items2)
-	{
-		this(view, items1, items2, null);
-	}
-
-
-	public MultiDataBoundRecyclerAdapter(BaseView view, ObservableArrayList<?> items1, ObservableArrayList<?> items2, ObservableArrayList<?> items3)
+	public MultiDataBoundRecyclerAdapter(BaseView view, ObservableArrayList<?>... items)
 	{
 		mView = view;
-		mItems1 = items1;
-		mItems2 = items2;
-		mItems3 = items3;
+		mItems = items;
 	}
 
 
@@ -49,22 +33,26 @@ abstract public class MultiDataBoundRecyclerAdapter extends BaseDataBoundRecycle
 	public int getItemCount()
 	{
 		int size = 0;
-		if(mItems1 != null) size += mItems1.size();
-		if(mItems2 != null) size += mItems2.size();
-		if(mItems3 != null) size += mItems3.size();
+		for(ObservableArrayList<?> list : mItems)
+		{
+			size += list.size();
+		}
 		return size;
 	}
 
 
 	public Object getItem(int position)
 	{
-		int size1 = mItems1 != null ? mItems1.size() : 0;
-		int size2 = mItems2 != null ? mItems2.size() : 0;
-		int size3 = mItems3 != null ? mItems3.size() : 0;
-
-		if(mItems1 != null && position < size1) return mItems1.get(position);
-		else if(mItems2 != null && position < size1 + size2) return mItems2.get(position - size1);
-		else if(mItems3 != null && position < size1 + size2 + size3) return mItems3.get(position - size1 - size2);
-		else return null;
+		int counter = 0;
+		for(ObservableArrayList<?> list : mItems)
+		{
+			if(position - counter - list.size() < 0)
+			{
+				int index = position - counter;
+				return list.get(index);
+			}
+			counter += list.size();
+		}
+		return null;
 	}
 }
